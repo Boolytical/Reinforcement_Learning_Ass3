07829a79e7da6b87e09b929114c7dea723f342d9
Felixkap <kapullafelix@gmail.com>
2022-04-29 08:42:14 +0200

    Add Helper and Reptitions

--- Assignment_3.py
+++ Assignment_3.py
@@ -1,21 +1,28 @@
 # https://towardsdatascience.com/policy-gradient-methods-104c783251e0
+import multiprocessing
+import os
+
 import gym
 import numpy as np
 import torch
+import time
+from Helper import LearningCurvePlot, smooth
+
 
 class REINFORCE_Agent:
     """ Agent that uses Monte Carlo Policy-Gradient Methods. """
+
     def __init__(self, env, param_dict: dict):
         """ Set parameters and initialize neural network and optimizer. """
         self.n_states = env.observation_space.shape[0]
         self.n_actions = env.action_space.n
         self.learning_rate = param_dict['alpha']
         self.gamma = param_dict['gamma']
-        
-        self.memory = []    # used for memorizing traces
-        
+
+        self.memory = []  # used for memorizing traces
+
         self.model = self._initialize_nn()
-        self.optimizer = torch.optim.Adam(self.model.parameters(), lr = self.learning_rate)
+        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=self.learning_rate)
 
     def _initialize_nn(self):
         """ Initialize neural network. """
@@ -28,12 +35,12 @@ class REINFORCE_Agent:
             torch.nn.Linear(256, self.n_actions),
             torch.nn.Softmax(dim=0))
 
-        return model    # return initialized model
-    
+        return model  # return initialized model
+
     def memorize(self, s, a, r):
         """ Memorize state, action and reward. """
         self.memory.append((s, a, r))
-        
+
     def forget(self):
         """ Empty memory. """
         self.memory = []
@@ -45,40 +52,38 @@ class REINFORCE_Agent:
         return action_probability, action
 
 
-
 def act_in_env(n_traces: int, n_timesteps: int, param_dict: dict):
-    env = gym.make('CartPole-v1')               # create environment of CartPole-v1
-    agent = REINFORCE_Agent(env, param_dict)    # initiate the agent
+    env = gym.make('CartPole-v1')  # create environment of CartPole-v1
+    agent = REINFORCE_Agent(env, param_dict)  # initiate the agent
 
-    env_scores = []                     # shows trace length over training time
+    env_scores = []  # shows trace length over training time
     for m in range(n_traces):
-        state = env.reset()             # reset environment and get initial state
+        state = env.reset()  # reset environment and get initial state
 
         # Use the policy to collect a trace
         for t in range(n_timesteps):
-            action_probability, action = agent.choose_action(s=state)   ## TODO action_probability not used yet, maybe consider using this instead of recalculating in line 87 ##
+            action_probability, action = agent.choose_action(s=state)  ## TODO action_probability not used yet, maybe consider using this instead of recalculating in line 87 ##
             state_next, _, done, _ = env.step(action)
-            agent.memorize(s=state, a=action, r=t+1)
-            
+            agent.memorize(s=state, a=action, r=t + 1)
+
             state = state_next
 
             if done:
-                env_scores.append(t+1)
+                env_scores.append(t + 1)
                 break
 
         # Estimate the return of the trace
-        R_t = torch.Tensor([r for (s, a, r) in agent.memory]).flip(dims=(0, )) ## Is this the R_t? print it ##
-        G_k = []                                            # Total return per timestep of the trace
+        R_t = torch.Tensor([r for (s, a, r) in agent.memory]).flip(dims=(0,))  ## Is this the R_t? print it ##
+
+        R_list = []
+        # Total return per timestep of the trace
         for i in range(len(agent.memory)):
-            new_G_k = 0
-            power = 0
-            for j in range(i, len(agent.memory)):
-                new_G_k = new_G_k + ((agent.gamma ** power) * R_t[j]).numpy()
-                power += 1
-            G_k.append(new_G_k)
-            
-        U_theta = torch.FloatTensor(G_k)                    # Expected return
-        U_theta /= U_theta.max()                            ## Why is this needed? ##
+            R_elements = [pow(agent.gamma, idx - i - 1) * R_t[idx - 1].numpy() for idx in
+                          range(i + 1, len(agent.memory) + 1)]
+            R_list.append(sum(R_elements))
+
+        U_theta = torch.FloatTensor(R_list)
+        #U_theta /= U_theta.max()  ## Why is this needed? -- I don't think it is needed##
 
         all_states = torch.Tensor(np.array([s for (s, a, r) in agent.memory]))
         all_actions = torch.Tensor(np.array([a for (s, a, r) in agent.memory]))
@@ -95,17 +100,75 @@ def act_in_env(n_traces: int, n_timesteps: int, param_dict: dict):
 
         if m % 50 == 0 and m > 0:
             print('Trace {}\tAverage Score: {:.2f}'.format(m, np.mean(env_scores[-50:-1])))
-            
+
         agent.forget()
-        
+
     env.close()
     return env_scores
 
 
 
-param_dict = {
-    'alpha': 0.001,             # Learning-rate
-    'gamma': 0.99               # Discount factor
-}
+def average_over_repetitions(n_repetitions, n_traces, n_timesteps, param_dict, smoothing_window):
 
-act_in_env(n_traces=1000, n_timesteps=500, param_dict=param_dict)
+    reward_results = np.empty([n_repetitions,n_traces]) # Result array
+    now = time.time()
+
+    for rep in range(n_repetitions):
+        print(f'Repetition: {rep}')
+        rewards = act_in_env(n_traces=n_traces, n_timesteps=n_timesteps, param_dict=param_dict)
+        reward_results[rep] = rewards
+
+    print('Running one setting takes {} minutes'.format((time.time() - now) / 60))
+    learning_curve = np.mean(reward_results, axis=0)  # average over repetitions
+    standard_error = np.std(reward_results, axis=0)
+    learning_curve = smooth(learning_curve, smoothing_window)  # additional smoothing
+    standard_error = smooth(standard_error, smoothing_window)
+    return learning_curve, standard_error
+
+
+def experiment( ):
+
+    n_repetitions = 20
+    n_traces = 1000
+    n_timesteps = 500
+
+    # param_dict = {
+    #     'alpha': 0.001,  # Learning-rate
+    #     'gamma': 0.99  # Discount factor
+    # }
+
+    smoothing_window = 101
+
+    ### Method: REINFORCE
+    method = 'REINFORCE'
+    gamma = 0.99
+    learning_rates = [0.001, 0.025, 0.01]
+    colours = ["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd", "#8c564b", "#e377c2", "#7f7f7f", "#bcbd22"]
+
+    run = 0
+    Plot = LearningCurvePlot(title=f'Method: {method} --- Results averaged of {n_repetitions} repetitions')
+    for alpha in learning_rates:
+
+            param_dict = {
+                'alpha': alpha,  # Learning-rate
+                'gamma': gamma  # Discount factor
+            }
+
+            print(f'Running {method}-method with learning rate={alpha} and discount rate={gamma}')
+
+
+            learning_curve, standard_error = average_over_repetitions(n_repetitions, n_traces, n_timesteps, param_dict, smoothing_window)
+            Plot.add_curve(x=np.arange(1, len(learning_curve)+1),
+                           y=learning_curve,
+                           std=standard_error,
+                           col=colours[run],
+                           label=r'REINFORCE with $\alpha$={} and $\gamma$={}'.format(alpha, gamma))
+            run += 1
+    Plot.save('REINFORCE.png')
+
+if __name__ == '__main__':
+    experiment()
+
+
+
+
---
+++ Helper.py
@@ -0,0 +1,47 @@
+'''
+An edited version of the learning curve plot from 'Reinforcement Learning' Assignment 1
+'''
+
+import matplotlib.pyplot as plt
+from scipy.signal import savgol_filter
+
+
+class LearningCurvePlot:
+
+    def __init__(self, title=None):
+        self.fig, self.ax = plt.subplots()
+
+        self.ax.set_xlabel('Trace')
+        self.ax.set_ylabel('Reward')
+
+        if title:
+            self.ax.set_title(title)
+
+    # Add new curve to the plot figure with given label name (optional) with rewards as y values
+    def add_curve(self, x, y, col, std=None, label=None):
+
+        if std is not None:
+            if label:
+                self.ax.plot(y, label=label)
+                self.ax.fill_between(x=x, y1=y-std, y2=y+std, color=col, alpha=0.2)
+            else:
+                self.ax.plot(y)
+                self.ax.fill_between(y1=y - std, y2=y + std, color=col,  alpha=0.2)
+        elif label:
+            self.ax.plot(y, label=label)
+        else:
+            self.ax.plot(y)
+
+    # Set the upper/lower bounds of the y-axis
+    def set_ylim(self, lower, upper):
+        self.ax.set_ylim([lower, upper])
+
+    # Save the plot figure given file name
+    def save(self, filename):
+        self.ax.legend(prop={'size': 7})
+        self.fig.savefig(filename, dpi=300)
+
+
+# Use Savitzky-Golayfilter for smoothing given y values
+def smooth(y, window, poly=1):
+    return savgol_filter(y, window, poly)
